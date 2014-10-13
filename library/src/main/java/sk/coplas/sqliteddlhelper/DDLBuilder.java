01215c762663842235c7b89303ecca4afe4720a7
Michal Pavlasek <pavlasek@gmail.com>
2014-10-13 21:02:30 +0200

    work in progress

--- library/src/main/java/sk/coplas/sqliteddlhelper/DDLBuilder.java
+++ library/src/main/java/sk/coplas/sqliteddlhelper/DDLBuilder.java
@@ -10,9 +10,6 @@ import java.util.List;
  */
 public class DDLBuilder {
 
-    public static enum ColumnType {
-        TEXT,NUMERIC,INTEGER,REAL,BOOLEAN, NONE
-    }
 
     private String tableName;
     private LinkedHashMap<String, ColumnType> columns;
@@ -41,70 +38,6 @@ public class DDLBuilder {
         return builder;
     }
 
-    public DDLBuilder pk(String columnName) {
-        this.primaryKey = columnName;
-        return this;
-    }
-
-    public DDLBuilder autoIncrement(String columnName) {
-        this.primaryKey = columnName;
-        return this;
-    }
-
-    public DDLBuilder pk() {
-        this.primaryKey = columnNameCache;
-        return this;
-    }
-
-    public DDLBuilder autoIncrement() {
-        this.autoincrement = columnNameCache;
-        return this;
-    }
-
-    public DDLBuilder notNull() {
-        this.notNullcolumns.add( columnNameCache);
-        return this;
-    }
-
-
-
-    public DDLBuilder text(String columnName) {
-        return column(columnName, ColumnType.TEXT);
-    }
-
-    public DDLBuilder numeric(String columnName) {
-        return column(columnName, ColumnType.NUMERIC);
-    }
-
-    public DDLBuilder integer(String columnName) {
-        return column(columnName, ColumnType.INTEGER);
-    }
-
-    public DDLBuilder real(String columnName) {
-        return column(columnName, ColumnType.REAL);
-    }
-
-    public DDLBuilder bool(String columnName) {
-        return column(columnName, ColumnType.BOOLEAN);
-    }
-
-    public DDLBuilder none(String columnName) {
-        return column(columnName, ColumnType.NONE);
-    }
-
-
-    public DDLBuilder column(String columnName, ColumnType columnType) {
-        if (isAlter && columns.size() > 0) {
-            throw new IllegalArgumentException("Only one column is allowed in SQLite ALTER");
-            //http://stackoverflow.com/questions/6172815/sqlite-alter-createTableBuilder-add-multiple-columns-in-a-single-statement
-        }
-
-        if (!columns.containsKey(columnName)) {
-            columns.put(columnName, columnType);
-        }
-        columnNameCache = columnName;
-        return this;
-    }
 
     public String build(){
         StringBuilder sb = new StringBuilder();
@@ -163,5 +96,105 @@ public class DDLBuilder {
             sb.append(" NOT NULL");
         }
     }
+
+    public static enum ColumnType {
+        TEXT,NUMERIC,INTEGER,REAL,BOOLEAN, NONE
+    }
+
+    private class SingleColumnBuilder{
+
+
+        private String name;
+        private ColumnType type;
+        private boolean pk;
+        private boolean autoIncrement;
+        private boolean notNull;
+
+        public DDLBuilder pk(String columnName) {
+            this.primaryKey = columnName;
+            return this;
+        }
+
+        public DDLBuilder autoIncrement(String columnName) {
+            this.primaryKey = columnName;
+            return this;
+        }
+
+        public DDLBuilder pk() {
+            this.primaryKey = columnNameCache;
+            return this;
+        }
+
+        public DDLBuilder autoIncrement() {
+            this.autoincrement = columnNameCache;
+            return this;
+        }
+
+        public DDLBuilder notNull() {
+            this.notNullcolumns.add( columnNameCache);
+            return this;
+        }
+
+
+
+        public DDLBuilder text(String columnName) {
+            return column(columnName, ColumnType.TEXT);
+        }
+
+        public DDLBuilder numeric(String columnName) {
+            return column(columnName, ColumnType.NUMERIC);
+        }
+
+        public DDLBuilder integer(String columnName) {
+            return column(columnName, ColumnType.INTEGER);
+        }
+
+        public DDLBuilder real(String columnName) {
+            return column(columnName, ColumnType.REAL);
+        }
+
+        public DDLBuilder bool(String columnName) {
+            return column(columnName, ColumnType.BOOLEAN);
+        }
+
+        public DDLBuilder none(String columnName) {
+            return column(columnName, ColumnType.NONE);
+        }
+
+
+        public DDLBuilder column(String columnName, ColumnType columnType) {
+            if (isAlter && columns.size() > 0) {
+                throw new IllegalArgumentException("Only one column is allowed in SQLite ALTER");
+                //http://stackoverflow.com/questions/6172815/sqlite-alter-createTableBuilder-add-multiple-columns-in-a-single-statement
+            }
+
+            if (!columns.containsKey(columnName)) {
+                columns.put(columnName, columnType);
+            }
+            columnNameCache = columnName;
+            return this;
+        }
+
+    }
+
+    private class MultiColumnBuilder extends SingleColumnBuilder{
+        private List<SingleColumnBuilder> columns;
+
+        private SingleColumnBuilder column;
+
+        private MultiColumnBuilder() {
+            columns = new ArrayList<SingleColumnBuilder>();
+        }
+
+        public MultiColumnBuilder column() {
+            if (column != null) {
+                columns.add( column);
+            }
+            column = new SingleColumnBuilder();
+            return this;
+        }
+
+    }
+
 
 }
